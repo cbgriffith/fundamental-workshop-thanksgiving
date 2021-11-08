aba6edd8e668a516c79158aa2eccd11f22d704d3
cbgriffith <c.b.griffith@outlook.com>
2021-11-08 15:13:10 -0500

    small changes to css

--- scripts/SideDishes/SideDishSelect.js
+++ scripts/SideDishes/SideDishSelect.js
@@ -1,7 +1,7 @@
 import { getSideDishes, useSideDishes } from "./SideDishDataProvider.js"
 import { PartyGuestList } from "../PartyGuests/PartyGuestList.js"
 
-const contentTarget = document.querySelector(".sideDish-list")
+const contentTarget = document.querySelector(".sideDish-select")
 
 export const SideDishSelect = () => {
     getSideDishes()
@@ -13,6 +13,7 @@ export const SideDishSelect = () => {
 
 const render = sideDishCollection => {
     contentTarget.innerHTML = `
+        <div><label>Sort by side dish</label></div>
         <select class="dropdown" id="sideDishSelect">
             <option value="0">Please select a side dish...</option>
             ${
@@ -29,8 +30,6 @@ const eventHub = document.querySelector("body")
 eventHub.addEventListener("change", (eventObject) => {
 
     if(eventObject.target.id === "sideDishSelect"){
-        console.log("You selected something from the side dish dropdown")
-        console.log("This is the side dish that was selected: ", eventObject.target.value)
         PartyGuestList("side-dish", eventObject.target.value)
     }
 })
